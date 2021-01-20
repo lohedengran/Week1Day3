5fe55e0a15fcdce2bef7d2efbcbc53885029b98a
Lo Hedengran <lohedengran@gmail.com>
2021-01-20 16:52:01 +0100

    Create Frukt.cs

---
+++ Frukt/Frukt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frukt
+{
+    class Frukt
+    {
+        public string FruktTyp { get; set; }
+        public string FruktNamn { get; set; }
+        public double PrisPerKg { get; set; }
+    }
+}
