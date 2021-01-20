c679c3d87d73ad83ff30676d54f73960e3ab5cdf
Lo Hedengran <lohedengran@gmail.com>
2021-01-20 14:44:08 +0100

    Update Program.cs
    exceptions

--- Week1Day3/Program.cs
+++ Week1Day3/Program.cs
@@ -6,7 +6,47 @@ namespace Week1Day3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Mata in en nämnare: ");
+            string keyboardInput = Console.ReadLine();
+            int nämnare = 0;
+
+            try
+            {
+                nämnare = int.Parse(keyboardInput);
+
+                if (nämnare != 0)
+                {
+                    int kvot = 20 / nämnare;
+                    Console.WriteLine($"{20} / {nämnare} = {kvot}");
+                }
+                else
+                    Console.WriteLine("Du kan inte dividera talet med 0");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Strängen har värdet null och kan inte konverteras.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Felaktig inmatning");
+
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"Talet måste vara ett tal mellan {int.MinValue} och {int.MaxValue}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Felaktiv inmatning!"); ;
+            }
+            finally
+            {
+                Console.WriteLine("Denna kodrad kommer ALLTID att köra oavsett om vi får ett exception eller inte.");
+            }
+
+
+
+            Console.WriteLine("Nu fortsätter programmet att köra!");
         }
     }
 }
