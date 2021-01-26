f2fc6359e4573a754814c7e08bfd0f8d5b497b24
Lo Hedengran <lohedengran@gmail.com>
2021-01-26 16:49:44 +0100

    Update Program.cs

--- Switch/Program.cs
+++ Switch/Program.cs
@@ -5,15 +5,10 @@ namespace Switch
     class Program
     {
 
-
-        static void VisaMeny()
-        {
-            Console.WriteLine("Gör ett val av alternativen nedan");
-            Console.WriteLine("Tryck I för insättning");
-            Console.WriteLine("Tryck U för uttag");
-            Console.WriteLine("Tryck S för saldo");
-            Console.WriteLine("Tryck A för avsluta");
-        }
+        const int antalTransaktioner = 3;
+        static string[] historik = new string[antalTransaktioner];
+        static int räknare = -1;
+        
         static void Main(string[] args)
         {
             int saldo = 0;
@@ -39,6 +34,10 @@ namespace Switch
                         Console.WriteLine("Du har valt saldo.");
                         VisaSaldo(saldo);
                         break;
+                    case ConsoleKey.T:
+                        Console.WriteLine("Du har valt transaktioner.");
+                        Transaktioner();
+                        break;
                     case ConsoleKey.A:
                         Console.WriteLine("Du har valt att avsluta.");
                         done = true;
@@ -50,6 +49,15 @@ namespace Switch
 
             }
 
+        }
+        static void VisaMeny()
+        {
+            Console.WriteLine("Gör ett val av alternativen nedan");
+            Console.WriteLine("Tryck S för saldo");
+            Console.WriteLine("Tryck I för insättning");
+            Console.WriteLine("Tryck U för uttag");
+            Console.WriteLine("Tryck T för transaktioner");
+            Console.WriteLine("Tryck A för avsluta");
         }
 
 
@@ -64,15 +72,21 @@ namespace Switch
         {
             Console.Clear();
             Console.WriteLine("Ange summa för insättning: ");
+            
+            
             int summaFörInsättning = int.Parse(Console.ReadLine());
+
             if (summaFörInsättning > 0)
             {
-                Console.WriteLine($"Summa att sätta in: {summaFörInsättning} kr.");
                 nuvarandeSaldo += summaFörInsättning;
+                historik[++räknare % antalTransaktioner]
+                    = $"Insättning {DateTime.Now.ToLongTimeString()}: {summaFörInsättning} kr.";
+                Console.WriteLine($"Summa att sätta in: {summaFörInsättning} kr.");
             }
             else
             {
                 Console.WriteLine("Ange en summa större än 0.");
+                GörInsättning(nuvarandeSaldo);
             }
             return nuvarandeSaldo;
         }
@@ -81,15 +95,34 @@ namespace Switch
             Console.Clear();
             Console.WriteLine("Ange summa för uttag: ");
             int summaFörUttag = int.Parse(Console.ReadLine());
-            if(summaFörUttag <= nuvarandeSaldo)
+            if(summaFörUttag <= nuvarandeSaldo && summaFörUttag > 0)
             {
                 Console.WriteLine($"Du har tagit ut {summaFörUttag} kr.");
                 nuvarandeSaldo -= summaFörUttag;
+                historik[++räknare % antalTransaktioner]
+                    = $"Uttag {DateTime.Now.ToLongTimeString()}: {summaFörUttag} kr.";
             } else 
             {
                 Console.WriteLine("Otillräckligt saldo på kontot");
             }
             return nuvarandeSaldo;
+        }
+
+        static void Transaktioner() // avoid index out of range error
+        {
+            Console.Clear();
+            //räknare--;
+            räknare += antalTransaktioner;
+
+            for (int i = 0; i < antalTransaktioner; i++)
+            {
+                string info = historik[räknare % antalTransaktioner];
+                räknare--;
+                if (info != null)
+                {
+                    Console.WriteLine($"{info}");
+                }
+            }
         }
     }
 }
